7910fd823123b8dfd0174dd1509c853c8831d65b
zhaowei <1512763623@qq.com>
2018-10-26 10:42:23 +0800

    bug

--- src/web/views/blog/assets/js/common.js
+++ src/web/views/blog/assets/js/common.js
@@ -1,13 +1,29 @@
+// 解码html字符串
 function rep(str) {
   str = "" + str;
   return str.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&apos;/g, "'");
 }
+// 根据node解码html字符串
 function baseNode(node) {
-  debugger
   var fuck = document.querySelectorAll(node);
-  debugger
   for (var i = 0; i < fuck.length; i++) {
-    debugger
     fuck[i].innerHTML = rep(fuck[i].innerHTML)
   }
 }
+// 添加window.onload方法
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldonload();
+      func();
+    }
+  }
+}
+
+// 统计rem单位计算
+addLoadEvent(function () {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth > 1200 ? '100px' : document.documentElement.clientWidth / 1200 * 100 + 'px';
+})
