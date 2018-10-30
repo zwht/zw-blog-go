1f52e3a5c08d4dcc19557ff4e0268ebddd238f06
zhaowei <1512763623@qq.com>
2018-10-30 23:44:10 +0800

    bug

--- src/tools/filters.go
+++ src/tools/filters.go
@@ -2,13 +2,39 @@ package tools
 
 import (
 	"html/template"
-	"time"
 )
+
+//截取字符串 start 起点下标 length 需要截取的长度
+func Substr(str string, start int, length int) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+	if start > end {
+		start, end = end, start
+	}
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+	return string(rs[start:end])
+}
 
 func RenderHtml(content string) template.HTML {
 	return template.HTML(content)
 }
-func DateFk(date string) string {
-	str, _ := time.Parse("2006-01-02 15:04:05", date)
-	return str.Format("2006-01-02 15:04:05")
+func DateFk(str string) string {
+	// str = "2018-10-30T23:18:11.673298Z"
+	return Substr(str, 0, 10) + " " + Substr(str, 11, 8)
 }
